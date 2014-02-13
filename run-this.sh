fc2bc92bf5a98f1f9747ea9d9ec8d7ffd118e55c
seanstory <sean.j.story@vanderbilt.edu>
2014-02-13 00:27:10 -0600

    darn permissions

--- run-this.sh
+++ run-this.sh
@@ -1,5 +1,5 @@
 #install Mongo for travisCI
-mkdir -p /data/db 
+sudo mkdir -p /data/db 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 #install new version
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
 sudo apt-get update
