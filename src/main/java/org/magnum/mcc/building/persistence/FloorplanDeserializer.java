46bb5c9fb9abe9d4d99579b15baa2794ce2cd49e
seanstory <sean.j.story@vanderbilt.edu>
2014-03-26 18:04:56 -0500

    fixed testing errors

--- src/main/java/org/magnum/mcc/building/persistence/FloorplanDeserializer.java
+++ src/main/java/org/magnum/mcc/building/persistence/FloorplanDeserializer.java
@@ -71,7 +71,12 @@ public class FloorplanDeserializer extends JsonDeserializer<Floorplan> {
 		String start = edge.get("start").textValue();
 		String end = edge.get("end").textValue();
 		Double len = edge.get("length").doubleValue();
-		String imageId = edge.get("imageId").textValue();
+		JsonNode tmp = edge.get("imageId");
+		String imageId;
+		if (tmp ==null)
+			imageId="";
+		else
+			imageId = tmp.textValue();
 
 		FloorplanLocation s = floorplan.locationById(start);
 		FloorplanLocation e = floorplan.locationById(end);
